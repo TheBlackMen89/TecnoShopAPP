d33b23d63874c601ee48fce3b9b32b38ed8bcb35
Gelber Quimbayo <gelberquimbayo@gmail.com>
2023-11-06 09:53:17 -0500

    Se modifica el Layout Registro de usuario

--- app/src/main/java/com/example/myapplication/activity_registro_usuarios.kt
+++ app/src/main/java/com/example/myapplication/activity_registro_usuarios.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class activiy_registro_usuarios : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registro_usuarios)
