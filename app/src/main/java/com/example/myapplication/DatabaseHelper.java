46d3699161dc73ba5112955a7156b1a4a0aa3cb1
Nicolas Gualtero <104392300+DAN7EN@users.noreply.github.com>
2023-11-18 22:08:53 -0500

    Se agregan iconos para la app, se crea firebase, se agrega la clase databasehelper

---
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package com.example.myapplication;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "LocationDB";
+    private static final String TABLE_NAME = "LocationTable";
+    private static final String COL_ID = "ID";
+    private static final String COL_LATITUDE = "Latitude";
+    private static final String COL_LONGITUDE = "Longitude";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " ("
+                + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COL_LATITUDE + " TEXT, "
+                + COL_LONGITUDE + " TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addLocation(String latitude, String longitude) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_LATITUDE, latitude);
+        contentValues.put(COL_LONGITUDE, longitude);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+}
+
+
